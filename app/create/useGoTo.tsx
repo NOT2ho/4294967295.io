03ac9fcd9517c755e93d5e472494fce410f0a59e
4294967295 <4294967295@snu.ac.kr>
2024-03-31 15:20:16 +0900

    Change icon

--- app/create/useGoTo.tsx
+++ app/create/useGoTo.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 
-export default function useGoTo(props) {
+export default function useGoTo(props: any) {
     const router = useRouter();
 
     router.refresh()
