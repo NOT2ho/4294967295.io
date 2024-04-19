23dae8dd82f6ec10313ec42ec8144ebf2ab8aa5f
4294967295 <4294967295@snu.ac.kr>
2024-04-19 23:35:06 +0900

    fix react doc issue

--- app/TejavaCalc/page.tsx
+++ app/TejavaCalc/page.tsx
@@ -1,34 +1,29 @@
 
 "use client"
 import React from 'react'
-import { useFormStatus } from 'react-dom'
-
+import { useState } from 'react'
+//import FormSubmit from './FormSubmit'
+import {useFormStatus} from 'react-dom'
 
 export default function TejavaCalc() {
-    const { pending } = useFormStatus()
-    return (
+   
+    const { pending, data } = useFormStatus()
+    const [val, setVal] = React.useState(0);
 
-        <form action={FormSubmit}  >
+    const FormSubmit = (formData: FormData) => {
+        const param = Number(formData.get('body'))
+        const die = 0.25 * param / 0.55 * 1000
+        console.log(die)
+
+        setVal(die)
+    }
+
+    return (       
+         <form action={FormSubmit}  >
             <p>
-                <textarea name="body" placeholder="체중 입력(숫자만)."></textarea>
+                <textarea name="body" placeholder="체중 입력(int, kg)."></textarea>
             </p>
-            <p><input type="submit" value="이상한거 쓰면 NaN 나옴" disabled={pending} /></p>
+            <p><input type="submit" value="NaN 나오면 문맹이라는 뜻임" disabled={pending} /></p><p>{val}</p>
         </form >
-        //
-
     )
-}
-
-
-
-
-function FormSubmit(formData: FormData) {
-
-    const param = Number(formData.get('body'))
-    const die = 0.25 * param / 0.55 * 1000
-
-    return <p>
-        당신은 데자와를 {die}캔 먹으면 0.5의 확률로 죽습니다. 주의하도록
-    </p>
-
 }
