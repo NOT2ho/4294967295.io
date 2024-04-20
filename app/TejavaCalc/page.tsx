a1707d17fa449ec76323bfb43d125e66de69fe1a
4294967295 <4294967295@snu.ac.kr>
2024-04-20 17:41:13 +0900

    fix kg issue

--- app/TejavaCalc/page.tsx
+++ app/TejavaCalc/page.tsx
@@ -3,25 +3,25 @@
 import React from 'react'
 import { useState } from 'react'
 //import FormSubmit from './FormSubmit'
-import {useFormStatus} from 'react-dom'
+import { useFormStatus } from 'react-dom'
 
 export default function TejavaCalc() {
-   
+
     const { pending, data } = useFormStatus()
     const [val, setVal] = React.useState(0);
     const [hide, setHide] = React.useState(false);
 
     const FormSubmit = (formData: FormData) => {
         const param = Number(formData.get('body'))
-        const die = 0.25 * param / 0.55 * 1000
+        const die = 0.25 * param / 0.55
         console.log(die)
 
         setVal(die)
         setHide(true)
     }
 
-    return (       
-         <form action={FormSubmit}  >
+    return (
+        <form action={FormSubmit}  >
             <p>
                 <textarea name="body" placeholder="체중 입력(int, kg)."></textarea>
             </p>
