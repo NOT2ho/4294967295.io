ac044f2c2209cf1b7d6da6020da015be77dcd20b
4294967295 <4294967295@snu.ac.kr>
2024-04-22 20:53:32 +0900

    fix path issue

--- app/AssumeLife/readFile.tsx
+++ app/AssumeLife/readFile.tsx
@@ -5,7 +5,7 @@ let die = 0
 export default async function readFile(Number: number) {
 
 
-    readXlsxFile("./life.xlsx").then((rows: any) => {
+    readXlsxFile("../life.xlsx").then((rows: any) => {
         console.log(rows);
 
         for (let i = 0; rows.flat(0)[i][0] != Number; i++) {
