3ec703d593663a9f0ea5c3802c8f90dbd4c14e45
4294967295 <4294967295@snu.ac.kr>
2024-04-25 16:02:26 +0900

    call function

--- app/meow/page.tsx
+++ app/meow/page.tsx
@@ -1,39 +1,38 @@
-"use client"
 import { ElevenLabsClient } from "elevenlabs";
 import * as dotenv from "dotenv";
 import { select } from "./select";
 
+export default async function meow() {
+    dotenv.config();
 
-dotenv.config();
+    const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
 
-const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
-
-if (!ELEVENLABS_API_KEY) {
-    throw new Error("Missing ELEVENLABS_API_KEY in environment variables");
-}
-
-const client = new ElevenLabsClient({
-    apiKey: ELEVENLABS_API_KEY,
-});
-const createAudioStreamFromText = async (
-    text: string
-): Promise<Buffer> => {
-    const audioStream = await client.generate({
-        voice: "Alice",
-        model_id: "eleven_monolingual_v1",
-        text,
-    });
-
-    const chunks: Buffer[] = [];
-    for await (const chunk of audioStream) {
-        chunks.push(chunk);
+    if (!ELEVENLABS_API_KEY) {
+        throw new Error("Missing ELEVENLABS_API_KEY in environment variables");
     }
 
-    const content = Buffer.concat(chunks);
-    return content;
-};
+    const client = new ElevenLabsClient({
+        apiKey: ELEVENLABS_API_KEY,
+    });
+    const createAudioStreamFromText = async (
+        text: string
+    ): Promise<Buffer> => {
+        const audioStream = await client.generate({
+            voice: "Alice",
+            model_id: "eleven_monolingual_v1",
+            text,
+        });
 
-export default async function meow() {
+        const chunks: Buffer[] = [];
+        for await (const chunk of audioStream) {
+            chunks.push(chunk);
+        }
+
+        const content = Buffer.concat(chunks);
+        return content;
+    };
+
+
     console.log(await select())
     await createAudioStreamFromText(await select())
 }
