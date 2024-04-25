0d4d704d79a7e3e3b7ebf5074e6286d0fa87f465
4294967295 <4294967295@snu.ac.kr>
2024-04-25 15:45:13 +0900

    fix redirect issue

--- app/MeowSound/formSubmit.tsx
+++ app/MeowSound/formSubmit.tsx
@@ -4,44 +4,47 @@ import pool from "../lib/db";
 
 export async function FormSubmit(formData: FormData) {
 
-    const param = formData.get('body')
-
-    let sttEng = ["ya", "ywe", "yweo", "wa", "ae", "i", "mi"]
-    let endEng = ["ong", "ung", "yong"]
-    let midEng = ["o", "u", "yo", "eu", "yu", "a", "i", "ae", "yea", "e"]
-
-    const Random = async (num: any) => {
-        const i = Number(num)
-
-
-        const getRandomInt = (Max: number) => {
-            return Math.floor(+Math.random() * +Max)
-        }
-
-        let strEng = sttEng[getRandomInt(sttEng.length - 1)]
-        if (i < 33) {
-
-
-            for (let j = 0; j < i; j++) {
-                strEng = strEng + midEng[getRandomInt(midEng.length - 1)]
-            }
-            strEng = strEng + endEng[getRandomInt(endEng.length - 1)]
-
-        }
-        else
-            strEng = "Keun su an dwoe"
-
-        return strEng;
-    }
-
 
     try {
+        const param = formData.get('body')
+
+        let sttEng = ["ya", "ywe", "yweo", "wa", "ae", "i", "mi"]
+        let endEng = ["ong", "ung", "yong"]
+        let midEng = ["o", "u", "yo", "eu", "yu", "a", "i", "ae", "yea", "e"]
+
+        const Random = async (num: any) => {
+            const i = Number(num)
+
+
+            const getRandomInt = (Max: number) => {
+                return Math.floor(+Math.random() * +Max)
+            }
+
+            let strEng = sttEng[getRandomInt(sttEng.length - 1)]
+            if (i < 33) {
+
+
+                for (let j = 0; j < i; j++) {
+                    strEng = strEng + midEng[getRandomInt(midEng.length - 1)]
+                }
+                strEng = strEng + endEng[getRandomInt(endEng.length - 1)]
+
+            }
+            else
+                strEng = "Keun su an dwoe"
+
+            return strEng;
+        }
+
         const sql = 'INSERT INTO nya (body) VALUES (?)';
         const result = await pool.query(sql, [await Random(param)]);
         console.log(result);
+
+        redirect('/meow')
+
     } catch (err) {
         console.log(err);
     }
 
-    redirect('/meow')
+
 }
