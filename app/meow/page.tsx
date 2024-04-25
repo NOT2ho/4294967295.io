62fb44593bb09c38114406f846b8c62e720a4917
4294967295 <4294967295@snu.ac.kr>
2024-04-25 18:01:04 +0900

    fix

--- app/meow/page.tsx
+++ app/meow/page.tsx
@@ -2,37 +2,36 @@ import { ElevenLabsClient } from "elevenlabs";
 import * as dotenv from "dotenv";
 import { select } from "./select";
 
+
+dotenv.config();
+
+const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
+
+if (!ELEVENLABS_API_KEY) {
+    throw new Error("Missing ELEVENLABS_API_KEY in environment variables");
+}
+
 export default async function meow() {
-    dotenv.config();
-
-    const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
-
-    if (!ELEVENLABS_API_KEY) {
-        throw new Error("Missing ELEVENLABS_API_KEY in environment variables");
-    }
-
     const client = new ElevenLabsClient({
         apiKey: ELEVENLABS_API_KEY,
     });
-    const createAudioStreamFromText = async (
-        text: string
-    ): Promise<Buffer> => {
-        const audioStream = await client.generate({
-            voice: "Alice",
-            model_id: "eleven_monolingual_v1",
-            text,
-        });
+    const audioStream = await client.generate({
+        stream: true,
+        voice: "Bella",
+        text: await select(),
+        model_id: "eleven_multilingual_v2"
+    });
+    const fs = require('fs');
 
-        const chunks: Buffer[] = [];
-        for await (const chunk of audioStream) {
-            chunks.push(chunk);
-        }
+    const writeStream = fs.createWriteStream
+    writeStream.pipe(`${await select()}`);
 
-        const content = Buffer.concat(chunks);
-        return content;
-    };
-
-
-    console.log(await select())
-    await createAudioStreamFromText(await select())
+    return (<p>
+        <body>
+            <button data-playing="false" role="switch" aria-checked="false">
+                <span>{await select()}</span>
+            </button>
+            <audio src={await select()}></audio>
+        </body></p>
+    )
 }
