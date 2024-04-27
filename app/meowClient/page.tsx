37ca47894b5f89f245a331dbe72765210f6e6e96
4294967295 <4294967295@snu.ac.kr>
2024-04-27 15:37:59 +0900

    sue vercel

--- app/meowClient/page.tsx
+++ app/meowClient/page.tsx
@@ -2,17 +2,15 @@
 
 import { select } from "../meowServer/select"
 import { useEffect } from "react";
-export default async function meow() {
-    const utterance = new SpeechSynthesisUtterance(await select());
-
-    function MeowClient() {
-        useEffect(() => {
+export default function MeowClient() {
+    useEffect(() => {
+        const meow = async () => {
             const mySynth = window.speechSynthesis;
-
+            const utterance = new SpeechSynthesisUtterance(await select());
             mySynth.speak(utterance)
-
-        })
-
+        }
+        meow()
     }
-    MeowClient()
+    )
+
 }
