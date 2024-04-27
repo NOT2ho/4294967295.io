b8d7cda6d12601abeec055581ede16ff2dd4db30
4294967295 <4294967295@snu.ac.kr>
2024-04-27 14:12:50 +0900

    <>

--- app/meowClient/page.tsx
+++ app/meowClient/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
                 애오오오오오옹
             </h1>
 
-            <ElevenLabsAudioNative publicUserId="7b255a2eb57cadc30fb58c8b845c1ab7ffc4be5a55c5db590a79b6026813f7c6" />
+            <ElevenLabsAudioNative publicUserId="<7b255a2eb57cadc30fb58c8b845c1ab7ffc4be5a55c5db590a79b6026813f7c6>" />
 
             <p>
                 와웅
