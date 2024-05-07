38fd559b6322edba62227a329c4ebee84e9b5949
4294967295 <4294967295@snu.ac.kr>
2024-05-07 11:07:40 +0900

    Auto stash before merge of "main" and "origin/main"

--- app/RegEx/page.tsx
+++ app/RegEx/page.tsx
@@ -2,16 +2,23 @@ export default async function RegExCreate() {
     const regex = /^t.?e+[^s-t]*/i
 
     const genRandStr = (num: number) => {
-        const arr =["아니","근데","진짜","ㅅㅂ","",""];
+        const arr =["아니","근데","진짜","뭐냐"];
         let res = '';
         for (let i = 0; i < num; i++) {
             res += arr[(Math.floor(Math.random() * arr.length))];
         }
         return res;
       }
+
+
+    const getRandNum = () => {
+        const num = Math.floor(Math.random()*100)
+        return num;
+    }      
     
     const callGenRandStr = () => {
-        console.log(genRandStr(30))
+        for  (let i=0; i<100; i++)
+        console.log(genRandStr(getRandNum()))
     }
     const Create = () => {
         {
