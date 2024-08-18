b4495a06f9c9a5b2c2716957182a27d6bcd4a6d7
4294967295 <4294967295@snu.ac.kr>
2024-08-19 02:51:48 +0900

    change input

--- app/markov/page.tsx
+++ app/markov/page.tsx
@@ -3,34 +3,35 @@ import markovCreate from '../markovCreate/page'
 
 
 export default async function ServerUploadPage() {
-  async function upload(data: FormData) {
+  async function upload(formData: FormData) {
     'use server'
 
-    const file: File | null = data.get('file') as unknown as File
+   /* const file: File | null = data.get('file') as unknown as File
     if (!file) {
       throw new Error('No file uploaded')
     }
 
     const bytes = await file.arrayBuffer()
     const buffer = Buffer.from(bytes)
-    
+    */
+    let txt : string | undefined = formData.get('content')?.toString()
+    let buf = txt ? Buffer.from(txt) : Buffer.from("")
+    console.log(txt)
     const path = '/tmp/tmp.txt'
-    await writeFile(path, buffer)
+    await writeFile(path, buf)
     console.log('file uploaded')
+    markovCreate()
     return { success: true }
   }
 
   return (
     <main>
       <h1>랜덤 문장 생성기: </h1>
-      <h2>*1만 자 이하*의 txt 파일을 업로드하세요. 파일이 충분히 크지 않으면 결과가 잘 안 나옵니다. 하지만 파일이 많이 크면 느립니다.<br></br>파일은 삭제됩니다. </h2>
+      <h2>*1만 자 이하*의 텍스트를 업로드하세요. 문자열이 충분히 길지 않으면 결과가 잘 안 나옵니다. 하지만 문자열이 많이 길면 느립니다.<br></br>입력은 삭제됩니다. </h2>
       <h3>정확히는 덮어씌워집니다.</h3>
       <form action={upload}>
-        <input type="file" name="file" />
-        <input type="submit" value="Upload" />
-      </form>
-            <form action={markovCreate}>
-        <button type="submit" >생성</button>
+        <textarea name="content" placeholder='^ _______ ^' /><br></br>
+        <button type="submit">생성(누르고 좀 기다려야 합니다)</button>
       </form>
     </main>
   )
