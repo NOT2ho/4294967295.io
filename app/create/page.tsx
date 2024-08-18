fe4d3749363339214b886d0098a2496f45105af9
4294967295 <4294967295@snu.ac.kr>
2024-08-18 13:03:42 +0900

    edit create

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -8,7 +8,7 @@ export default function Create() {
     return (
         <form action={FormSubmit} >
             <p>
-                <textarea name="body" placeholder="여기."></textarea>
+                <textarea name="body" placeholder="입력한 것은 제 자연어처리 데이터셋으로 쓰일 수도 있습니다."></textarea>
             </p>
             <p><input type="submit" value="ㄱㄱ" /></p>
         </form >
--- app/read2/page.js
+++ app/read2/page.js
@@ -9,11 +9,10 @@ export default async function read() {
   }
 
 
-  return (<form name="read" action={Read2}>
-    <input type="range" id="body" name="body" min="0" max={await readRange()} />
-    <input type="submit"></input>
-    <>{Read2}</>
-  </form>
+  return (
+<>안타깝게도 아직 작성한 글을 볼 수 있는 기능은 구현되지 않았습니다<br></br>
+      하지만 여러분의 소중한 의견은 제 db에 잘 저장되므로 저는 읽을 수 있으니 걱정하지 않으셔도 됩니다!<br></br> 4294967295 올림.</>
+    
   )
 
 
