4d80303cdfddbbb9d72ad4024e59041c9bf67b39
4294967295 <4294967295@snu.ac.kr>
2024-08-19 02:54:34 +0900

    fix server action issue

--- app/markov/page.tsx
+++ app/markov/page.tsx
@@ -20,7 +20,6 @@ export default async function ServerUploadPage() {
     const path = '/tmp/tmp.txt'
     await writeFile(path, buf)
     console.log('file uploaded')
-    markovCreate()
     return { success: true }
   }
 
@@ -31,7 +30,10 @@ export default async function ServerUploadPage() {
       <h3>정확히는 덮어씌워집니다.</h3>
       <form action={upload}>
         <textarea name="content" placeholder='^ _______ ^' /><br></br>
-        <button type="submit">생성(누르고 좀 기다려야 합니다)</button>
+        <button type="submit">이 버튼을 먼저 누르고 생성 버튼을 누르십시오</button>
+    </form>
+            <form action={markovCreate}>
+        <button type="submit" >생성(누르고 좀 기다리십시오)</button>
       </form>
     </main>
   )
