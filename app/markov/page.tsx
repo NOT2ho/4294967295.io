0a591401a016a443727fd5c5a0213accf8f05a95
4294967295 <4294967295@snu.ac.kr>
2024-08-21 23:06:09 +0900

    eliminate dependency

--- app/markov/page.tsx
+++ app/markov/page.tsx
@@ -26,16 +26,16 @@ export default async function ServerUploadPage() {
 
   return (
     <main>
-      <h1>랜덤 문장 생성기: 504 gateway timeout </h1>
-      <h2>많이 긴 문자열을 입력하세요. 근데 형태소 분석기가 너무 느려서 504가 뜹니다<br></br>입력은 삭제됩니다. </h2>
+      <h1>랜덤 문장 생성기: 이제 빠름</h1>
+      <h2>많이 긴 문자열을 입력하세요. 제가 엄청 빠른 거 만들어서 이제 긴 거 넣어도 잘됩니다. 아마도<br></br>입력은 삭제됩니다. </h2>
       <h3>정확히는 덮어씌워집니다.</h3>
-      <>etri가 저를 차단했습니다. 그래서 느립니다.</>
+      <>etri가 저를 차단했습니다. 그래서 <a href='https://www.npmjs.com/package/notpos_kr>'>형태소 분석기</a>를 만들었습니다.</>
       <form action={upload}>
         <textarea name="content" placeholder='^ _______ ^' /><br></br>
         <button type="submit">이 버튼을 먼저 누르고 생성 버튼을 누르십시오</button>
     </form>
             <form action={markovCreate}>
-        <button type="submit" >생성(누르고 페이지가 넘어갈 때까지 기다리십시오. 엄청나게 오래 걸립니다.)</button>
+        <button type="submit" >생성(누르고 페이지가 넘어갈 때까지 기다리십시오.)</button>
       </form>
     </main>
   )
--- app/markovCreate/page.tsx
+++ app/markovCreate/page.tsx
@@ -1,9 +1,9 @@
 'use server'
 import { promises as fs } from 'fs'
+import fss from 'fs'
 import { nGram } from "simplengrams";
 import pool from "../lib/db";
 import path from 'path'
-import { Pos } from 'notpos_kr';
 
 
 import { redirect } from 'next/navigation'
@@ -34,11 +34,12 @@ export default async function markovCreate() {
             }
 
             
-                //const regEx = /'^J|^E|^X|^S'/
+            //const regEx = /'^J|^E|^X|^S'/
+            
                 let res = sentence.join(' ')
                 return res
             }
-            ////console.log(data)
+            console.log(data)
    
             async function calc_cfd() {
                 let cfd = {};
@@ -56,13 +57,15 @@ export default async function markovCreate() {
                 
                 
                                 let sentence = sentences.join(' ')
-                        ngrams = nGram(sentence);
+                ngrams = nGram(sentence);
+                ngrams[0][0] = ngrams[ngrams.length-1][1]
                 for (let arrs in ngrams) {
                     let arr = ngrams[arrs]
                     ////console.log(arr)
                     if (arr[0] == null) {
                         throw "ngram issue"
                     }
+                    
                     let w1: string = arr[0]
                     Object.assign(cfd, { [w1]: [] })
                     for (let arrs2 in ngrams) {
@@ -103,3 +106,150 @@ export default async function markovCreate() {
 
 
 
+class Node {
+    child: Map<string, Node>;
+    output: string[][]
+    fail: null | Node
+    end : boolean
+    
+    constructor() {
+        this.child = new Map()
+        this.output = []
+        this.fail = null
+        this.end = false
+    }
+}
+
+class AhoCorasick {
+    root: Node
+    constructor() {
+        this.root = new Node();
+    }
+
+    insert(words: string[]) {
+        let output = words
+        let word = words[0]
+        let node = this.root;
+        for (const char of word) {
+            if (!node.child.get(char))
+                node.child.set(char, new Node());
+            node = node.child.get(char) || this.root;
+        }
+        node.output.push(output);
+        node.end = true
+    }
+
+
+    fail() {
+        const que : Node[] = []
+        for (const [i, c] of this.root.child.entries()) {
+            c.fail = this.root;
+            que.push(c);
+          
+            while (que.length > 0) {
+                const currentNode = que.shift();
+                if (typeof currentNode === undefined)
+                    break;
+                            
+                for (const i in currentNode?.child) {
+                    const nextNode = currentNode?.child.get(i);
+
+                    if (nextNode == null)
+                        continue;
+                
+                    que.push(nextNode);
+
+                    let failNode = currentNode.fail;
+
+                    while (failNode !== null && !failNode.child.get(i)) {
+                        failNode = failNode.fail;
+                    }
+                
+                    if (currentNode != this.root)
+                        nextNode.fail = failNode ? failNode.child.get(i) || this.root : this.root;
+
+                    if (nextNode.fail !== null)
+                        nextNode.output = nextNode.output.concat(nextNode.fail.output);
+                }
+        
+            }
+        }
+    }
+
+    search(input: string) : Map<number, string[][]> {
+        this.fail();
+        let text = input;
+        let result : Map<number, string[][]> = new Map();
+        let currentNode : Node | null = this.root;
+        for (let i = 0; i < text.length; i++) {
+            const char = text[i];
+
+            while (currentNode !== null && !currentNode.child.get(char)) {
+                currentNode = currentNode.fail;
+            }
+    
+            currentNode = currentNode ? currentNode.child.get(char) || this.root : this.root;
+        
+            for (const output of currentNode.output) {
+                let resultArray = result.get(i - output[0].length + 1) || [];
+                resultArray.push(output);
+                result.set(i - output[0].length + 1, resultArray);
+            }   
+        }
+        return result;
+    }
+
+}
+
+class Pos {
+    preprocess = (text: string) => {
+        const str = text.replace(/([^가-힣a-zA-Z]*)/, " ")
+        const undefArr = str.split(' ')
+        const arr = undefArr.splice(1, undefArr.length)
+        return arr.toString();
+    }
+
+    tag = (text: string) => {
+        const ac = new AhoCorasick();
+        let res : Map<number, string[][]> = new Map();
+        try {
+            const data = fss.readFileSync(path.join(process.cwd(), '/node_modules/notpos_kr/dic/dic.csv'))
+            const pd = data.toString().split('\n');
+            for (let i of pd) {
+                let word = i.slice(0, -1).split(',');
+                ac.insert(word);
+            }
+            res = ac.search(text);
+
+            const result : Map<number, string[]> = new Map();
+            for (let [i, x] of res.entries()) {
+                const resultArray = x[x.length - 1] || [];
+                result.set(i, resultArray);
+            }
+       
+            let idx = 0
+            let key = 0
+            let ret : Map<number, string[]> = new Map();
+            let keys = Array.from(result.keys());
+            for (let s of text)
+            {
+                if (idx >= text.length) break;
+                key = keys[idx];
+                const resv = result.get(idx) || []
+                if (resv.length == 0) {
+                    if (text[idx] != ' ') {
+                        ret.set(idx, [text[idx], 'UNK']);
+                    }
+                    idx++;
+                } else {
+                    ret.set(idx, resv);
+                    idx += resv[0].length;
+                }
+            }
+                
+            return Array.from(ret.values());
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
