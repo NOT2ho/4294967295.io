e3154852a7aa88668a1070b58956700e90aefd68
not2ho <4294967295@snu.ac.kr>
2024-12-06 15:44:42 +0900

    add link

--- app/formalLove/page.tsx
+++ app/formalLove/page.tsx
@@ -1,8 +1,8 @@
 
 export default async function formalLove() {
     return (<>
-        펀딩 바로가기 : https://tumblbug.com/formallove
-        텍스트 어드벤처 체험판 다운로드 : https://github.com/NOT2ho/formalLove-trial/releases
-        재미있는 테스트: https://4294967295.io/formalLoveTest
+        <a href="https://tumblbug.com/formallove">펀딩 바로가기</a><br></br>
+        <a href="https://github.com/NOT2ho/formalLove-trial/releases">텍스트 어드벤처 체험판 다운로드</a><br></br>
+        <a href="https://4294967295.io/formalLoveTest">재미있는 테스트</a><br></br>
     </>)
 }
